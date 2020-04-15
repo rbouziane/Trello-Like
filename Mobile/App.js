16fdb8055f106cc53a19e4e26d37efbad4cbeece
tbelhadj <tbelhadj@localhost.localdomain>
2020-04-15 13:26:50 +0200

    test of flying card + home page and description

--- Mobile/App.js
+++ Mobile/App.js
@@ -6,14 +6,18 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    SafeAreaView,
+    Animated,
+    PanResponder,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    Button,  
+    StatusBar,
+    Alert,
 } from 'react-native';
 
 import {
@@ -24,14 +28,28 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+function Separator() {
+    return <View style={styles.separator} />;
+}
+
 const App: () => React$Node = () => {
+    const pan = useRef(new Animated.ValueXY()).current;
+    const panResponder = useRef(
+    PanResponder.create({
+        useNativeDriver:  () => true, 
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([ null,
+        { dx: pan.x, dy: pan.y }
+      ]),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, { toValue: { x: 0, y: 0 } }).start();
+      }
+    })
+  ).current;
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
@@ -40,75 +58,82 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>Welcome</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+          This is the first version of  <Text style={styles.highlight}>My_Trello</Text>, more changes
+                and features are coming for this new app.
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
+          <Separator />
+          <View style={styles.container}>
+          <View style={styles.buttonContainer}>
+          <Button
+      title="Show Description"
+      color="#6495ed"
+      onPress= {() => Alert.alert('This application in the future will be used like Trello to help you organize your work.')} />
+          </View>
+          </View>
+          <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }]
+        }}
+        {...panResponder.panHandlers}
+      >
+        <View style={styles.box} />
+      </Animated.View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    box: {
+        height: 150,
+        width: 150,
+        backgroundColor: "blue",
+        borderRadius: 5,
+        alignItems: "center"
+    },
+    buttonContainer: {
+        margin: 40,
+    },
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: 'absolute',
+        right: 0,
+    },
+    Body: {
+        backgroundColor: Colors.white,
+    },
+    sectionContainer: {
+        marginTop: 120,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 32,
+        fontWeight: '600',
+        color: Colors.black,
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+        color: Colors.dark,
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+    },
 });
 
 export default App;
