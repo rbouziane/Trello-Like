6379e5f07ee0a4c0053d7276838c21c202af33d1
Gaspard Mater <gaspard.mater@epitech.eu>
2020-04-21 18:24:06 +0200

    [ADD] header component and modif color background

---
+++ web/trello/src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+isAuth: boolean;
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    this.isAuth = true;
+  }
+  SignOut() {
+    this.isAuth = false;
+  }
+}
